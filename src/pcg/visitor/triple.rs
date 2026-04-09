b3223cef601a47fe2421de75ed40bb502ea094be
Thomas Mayerl <tmayerl@student.ethz.ch>
2026-04-09 14:28:27 +0200

    Remove delegation edges upon storage dead (dangling ptrs)

--- src/pcg/visitor/triple.rs
+++ src/pcg/visitor/triple.rs
@@ -1,9 +1,7 @@
 use crate::{
-    error::PcgError,
-    pcg::{
-        obtain::ObtainType, triple::{PlaceCondition, Triple}
-    },
-    utils::DataflowCtxt,
+    borrow_pcg::edge::kind::BorrowPcgEdgeKind, error::PcgError, pcg::{
+        PcgRefLike, obtain::ObtainType, triple::{PlaceCondition, Triple}
+    }, utils::{DataflowCtxt, Place}
 };
 
 use super::PcgVisitor;
@@ -27,6 +25,23 @@ impl<'a, 'tcx: 'a, Ctxt: DataflowCtxt<'a, 'tcx>> PcgVisitor<'_, 'a, 'tcx, Ctxt> 
                 }
                 self.place_obtainer()
                     .obtain(local.into(), ObtainType::ForStorageDead)?;
+
+                // With raw pointers, we can have dangling pointers
+                // Remove any incoming delegation edges
+                let place: Place = local.into();
+                let blocking_edges = self.pcg.borrows_graph().edges_blocking(place.into(), self.ctxt.bc_ctxt());
+                
+                let mut to_remove = vec![];
+                for edge in blocking_edges {
+                    match edge.kind {
+                        BorrowPcgEdgeKind::Delegation(_) => {to_remove.push(edge.kind().clone())}
+                        _ => {}
+                    }
+                };
+
+                for edge in to_remove {
+                    self.pcg.borrow.graph.remove(&edge);
+                }
             }
             PlaceCondition::Unalloc(_) | PlaceCondition::Return => {}
             PlaceCondition::RemoveCapability(_) => unreachable!(),
