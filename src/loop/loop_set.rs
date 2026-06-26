39a3e260cd6dc25ed9dc6d7abd44c7dd49f021ba
Zack Grannan <zgrannan@protonmail.com>
2026-06-26 08:06:55 -0700

    fix loop bug

--- src/loop/loop_set.rs
+++ src/loop/loop_set.rs
@@ -17,15 +17,6 @@ impl LoopSet {
         self.data.insert(loop_idx);
     }
 
-    /// Adds all loops in `other` to this set.
-    pub(super) fn merge(&mut self, other: &Self) {
-        self.data.extend(other.data.iter().copied());
-    }
-
-    /// Removes the loop from the set.
-    pub(super) fn remove(&mut self, loop_idx: LoopId) {
-        self.data.remove(&loop_idx);
-    }
     pub(super) fn contains(&self, loop_idx: LoopId) -> bool {
         self.data.contains(&loop_idx)
     }
