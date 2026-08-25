be8da5b28c17d69254a8aabc154132092811ed13
Zack Grannan <zgrannan@protonmail.com>
2026-08-25 13:13:13 -0700

    small server tweak

--- pcg-server/start-server.sh
+++ pcg-server/start-server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This script is used to start the pcg-server in a Docker container
 export LD_LIBRARY_PATH=$(rustc --print sysroot)/lib:$LD_LIBRARY_PATH
-exec target/release/pcg-server "$@"
+exec ${CARGO_TARGET_DIR:-target}/release/pcg-server "$@"
 
